4c72104a73a96bc6464e714e59a3082f9d69a886
Martijn <martijn.niji@gmail.com>
2016-04-27 17:39:34 -0400

    Applied fixes from StyleCI

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * Copyright Martijn van der Kleijn, 2015
+ * Licensed under MIT license.
+ */
+
 ini_set('include_path', ini_get('include_path')
                         .PATH_SEPARATOR.dirname(__FILE__).'/src/');
 
